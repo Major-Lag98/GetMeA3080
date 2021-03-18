e679a3eedd8ae44499701a0e33dbe0832f3ff7f1
Alden <aldensmith06@Gmail.com>
2021-03-18 12:54:47 -0700

    Fixed minor bugs, adjusted values, and added some edge cases

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
-PATH = "C:\Program Files (x86)\SeleniumDrivers\chromedriver.exe"
+PATH = "C:\Program Files (x86)\SeleniumDrivers\chromedriver.exe"    # Make this the path to the driver exe of your choice
 
 driver = webdriver.Chrome(PATH)
 
@@ -15,7 +15,13 @@ email = "email@email.com"
 password = "password"
 priceLimit = 800
 
+refresh_if_too_expensive = True     # If false program will instead just exit
+
+itemURL = "https://www.amazon.com/dp/B08HR5SXPS?smid=ATVPDKIKX0DER&tag=data20-20#aod"   # Item page you wish to watch
+
+#
 # first sign in with email and password
+#
 driver.get("https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&")
 
 EmailFieldFound = False
@@ -23,7 +29,7 @@ completedSignIn = False
 
 print("Signing in")
 
-while not EmailFieldFound or completedSignIn:
+while not EmailFieldFound or not completedSignIn:
 
     inputField = driver.find_elements_by_id("ap_email")
 
@@ -33,7 +39,6 @@ while not EmailFieldFound or completedSignIn:
         field = inputField[0]
         field.send_keys(email)
         field.send_keys(Keys.RETURN)
-        # time.sleep(3)
 
         PasswordFieldFound = False
 
@@ -46,6 +51,7 @@ while not EmailFieldFound or completedSignIn:
                 field = inputField[0]
                 field.send_keys(password)
                 field.send_keys(Keys.RETURN)
+                completedSignIn = True
                 time.sleep(1)
             else:
                 print("Password not found")
@@ -53,48 +59,51 @@ while not EmailFieldFound or completedSignIn:
                 break
     else:
         print("Email not found")
-        time.sleep(100)
+        time.sleep(1)
         driver.refresh()
 
 print("Sign in complete")
 
 
-# Test a link that's unavailable and one that's available
+driver.get(itemURL)     # After signing in go to items page for watching
 
-# Bad link
-#driver.get("https://www.amazon.com/dp/B08HH5WF97?smid=ATVPDKIKX0DER&tag=data20-20#aod")
-
-# Good link expensive
-#driver.get("https://www.amazon.com/Gaming-GeForce-Graphics-DisplayPort-Bearings/dp/B08YBB228Y/ref=sr_1_1_sspa?crid=3MTLIS53NPC42&dchild=1&keywords=rtx+3080&qid=1616024327&sprefix=rtx%2Caps%2C230&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyUU4yRzZLT1VXWUxPJmVuY3J5cHRlZElkPUEwMTgwNDQ0M0NPSUlXQzlSUTlVUSZlbmNyeXB0ZWRBZElkPUEwOTI4MzIyMkNSN1lES1U4NUlWQyZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=")
-
-# good link cheap
-#driver.get("https://www.amazon.com/Paulas-Choice-SKIN-PERFECTING-Exfoliant-Facial-Blackheads/dp/B07C5SS6YD/ref=sr_1_13?dchild=1&keywords=exfoliation%2Bcream&qid=1616032354&sr=8-13&th=1")
-
-driver.get("https://www.amazon.com/dp/B07C5SS6YD/ref=twister_B01JGVQ3SQ?_encoding=UTF8&psc=1")
-
-
+#
 # second check price
-priceFound = False
+#
+appropriatePriceFound = False
 price = 0
 priceIDNames = ["priceblock_ourprice", "price_inside_buybox"]
 searchIndex = 0
 print("Checking price")
-while not priceFound:
+while not appropriatePriceFound:
 
     priceText = driver.find_elements_by_id(priceIDNames[searchIndex])
     if len(priceText) > 0:
-        priceFound = True
-        price = price_dec(priceText[0].text)
+        appropriatePriceFound = True
+        price = price_dec(priceText[0].text)    # parse the price string and convert to decimal
         if price < priceLimit:
-            print("Price is nominal")
+            print(f"Price is ${price}, which is appropriate")
         else:
-            print("Too expensive")
-            exit(-1)
-    else:
-        print(priceIDNames[searchIndex] + " not found. Trying next")
+            print(f"${price} is too expensive for the limit of ${priceLimit}")
+            if refresh_if_too_expensive:
+                driver.refresh()
+                print("Refreshing")
+                appropriatePriceFound = False
+            else:
+                exit(-1)
+    elif searchIndex < len(priceIDNames) - 1:
+        #print(f"{priceIDNames[searchIndex]} not found. Trying next")   # Debug
         searchIndex += 1
+    else:
+        #print(f"{priceIDNames[searchIndex]} not found.")    # Debug
+        print("Didn't find price, Item unavailable")
+        print("Refreshing")
+        driver.refresh()
+        searchIndex = 0
 
+#
 # Third, try to buy
+#
 
 buttonFound = False
 orderConfirmed = False
@@ -108,14 +117,16 @@ while not buttonFound or not orderConfirmed:
         buttonFound = True
         print("Buying")
         addToCartButton[0].click()
-        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "turbo-checkout-iframe")))
-        driver.switch_to.frame(driver.find_element_by_id("turbo-checkout-iframe"))
 
-        #Fourth, confirm order
+        #
+        #Fourth, confirm your order
+        #
 
         while not orderConfirmed:
 
-            #driver.switch_to.frame()
+            #wait for the confirm iframe to pop up #with a timeout of 5seconds
+            WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.ID, "turbo-checkout-iframe")))
+            driver.switch_to.frame(driver.find_element_by_id("turbo-checkout-iframe"))
             placeOrderButton = driver.find_elements_by_id("turbo-checkout-pyo-button")
 
             print("The length is " + str(len(placeOrderButton)))
@@ -130,7 +141,7 @@ while not buttonFound or not orderConfirmed:
                 break
 
     else:
-        time.sleep(3)
+        time.sleep(1)
         driver.refresh()
 
 print("All done")
